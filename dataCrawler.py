48a4c989c4fa11e8e100b59c27c211e31f16d90a
Cas Bertrams <cas.bertrams@vennster.nl>
2021-10-11 12:21:37 +0200

    Added commentary and some cleaning

--- dataCrawler.py
+++ dataCrawler.py
@@ -2,23 +2,24 @@ import csv
 import requests
 import json
 import urllib.request
-import glob
 import os
 from bs4 import BeautifulSoup
-import numpy as np
 import pandas as pd
 
 #### CONFIG ####
 searchQuery = 'mensenhandel'
-baseUrl = 'https://uitspraken.rechtspraak.nl/api/zoek'
-LiDoBaseUrl = 'https://linkeddata.overheid.nl/front/portal/document-viewer?ext-id='
+baseUrl = 'https://uitspraken.rechtspraak.nl/api/zoek' 
+LiDoBaseUrl = 'https://linkeddata.overheid.nl/front/portal/document-viewer?ext-id=' ## Not used anymore but can't hurt to keep. LiDo is practically the same site as the uitsprakenBaseUrl
 uitsprakenBaseUrl = 'https://uitspraken.rechtspraak.nl/inziendocument?id='
-case_count = 4000
-save_text_location = os.getcwd() + '/data/'
+case_count = 5  ## Amount of cases to retrieve. If you want max, just do an insanely high number, it stops when it fetched all cases. Backend does not accept values like 'max'.
+save_text_location = os.getcwd() + '/data/' # You have to have created this folder first. 
 ################
 
 cases_df = pd.DataFrame()
     
+### Method to extract the text from the casefiles. Search the uitsprakenBaseURL with case ID to get the html file. 
+### Filter through the html file with BeautifulSoup and look for either class 'uitspraak' or 'conclusie' as the standard seems to switch between cases. 
+### Parse html file to textfile to get a clean text file. Replace \n and commas with empty strings.
 def getCaseText(caseId):
     response = requests.get(uitsprakenBaseUrl + caseId)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -32,21 +33,20 @@ def getCaseText(caseId):
     uitspraak = uitspraak.replace(',', '')
     return uitspraak
 
-
+### Method to parse each case individually. Save case attributes to cases.csv and save each case text to a .txt file in /data/ folder and name it the case ID.
 def parseCaseInfo(results):
     global cases_df
     for case in results:
         caseText = getCaseText(case['TitelEmphasis'])
-        # print(caseText[0:100])
-        #case['Uitspraak'] = caseText
-        parsedId = case['TitelEmphasis'].replace(':', '-')
-        f= open(save_text_location + parsedId + ".txt","w+", encoding='utf-8')
+        parsedId = case['TitelEmphasis'].replace(':', '-') # Case id's apparentlyl use ':'. We cannot save files with ':' in the name, so we replace them with '-'.
+        f= open(save_text_location + parsedId + ".txt","w+", encoding='utf-8') # Doesn't work without encoding. 
         f.write(caseText)
         case['Case ID'] = parsedId
         cases_df = cases_df.append(case, ignore_index = True)
         print('Processed case', case['Case ID'])
     cases_df.to_csv('cases.csv', index=False)
 
+### Method to retrieve raw data from the backend. Backend url is at the top in the Config. files = complete request payload the backend expects to receive. 
 def queryUitspraak():
     print("Querying")
     files = {"StartRow":0,"PageSize":case_count,"ShouldReturnHighlights":'true',"ShouldCountFacets":'true',"SortOrder":"Relevance","SearchTerms":[{"Term":searchQuery,"Field":"AlleVelden"}],"Contentsoorten":[],"Rechtsgebieden":[],"Instanties":[],"DatumPublicatie":[],"DatumUitspraak":[],"Advanced":{"PublicatieStatus":"AlleenGepubliceerd"},"CorrelationId":"9abc658b0ce64f8786992af6965aabc4","Proceduresoorten":[]}
