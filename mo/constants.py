7d579512c3af89d130dfca07ec88c149a7709edb
Rens660 <rensjh@gmail.com>
2022-01-24 15:38:08 +0100

    added constants file

---
+++ mo/constants.py
@@ -0,0 +1,19 @@
+DRUGS_AND_PRECURSORS = {
+    "safrol": ['safrol', 'isosafrol', 'safrool', 'isosafrool'],
+    "piperonal": ['piperonal', 'piperonalglycidaat'],
+    "PMK": ['PMK','piperonylmethylketon'],
+    "BMK": ['BMK', 'BenzylMethylKeton'],
+    "apaan": ['apaan','Alpha-Phenylacetoacetonitrile', 'AlphaPhenylacetoacetonitrile'],
+    "GBL": ['GBL', 'Gamma-butyrolacton'],
+    "MDP2P": ['MDP2P'],
+    "efedrine": ['Efedrine', 'Ephedrine'],
+    "ergotamine": ['Ergotamine'],
+    "lyserginezuur": ['Lyserginezuur'],
+    "methamphetamine": ['Methamfetamine', 'Methamphetamine'],
+    "MDMA": ['MDMA', 'Methylenedioxymethamphetamine'],
+    "amphetamine": ['Amfetamine', 'Amphetamine'],
+    "cocaine": ['Cocaïne', 'Cocaine', 'Coke'],
+    "XTC": ['XTC', 'ecstasy'],
+    "GHB": ['GHB', 'gamma-hydroxyboterzuur', '4-hydroxybutaanzuur']
+}
+
