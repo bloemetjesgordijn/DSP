dd07e5d1aec662227ace1de1673f208fe8d0d651
Rens660 <rensjh@gmail.com>
2022-01-24 19:56:14 +0100

    .

--- mo/constants.py
+++ mo/constants.py
@@ -17,3 +17,53 @@ DRUGS_AND_PRECURSORS = {
     "GHB": ['GHB', 'gamma-hydroxyboterzuur', '4-hydroxybutaanzuur']
 }
 
+
+a = '''CREATE TABLE IF NOT EXISTS public.court_verdicts (
+        case_id varchar(100) NOT NULL,
+        case_text text,
+        PRIMARY KEY (case_id)
+    )'''
+
+b = '''CREATE TABLE IF NOT EXISTS public.case_data (
+        case_id varchar(100) NOT NULL,
+        titel varchar(255),
+        court_datum date,
+        uitspraak_type varchar(30),
+        gerechtelijk_product_type varchar(30),
+        procedure_soort varchar(100),
+        rechtsgebieden varchar(100),
+        PRIMARY KEY (case_id)
+    )'''
+
+c = '''CREATE TABLE IF NOT EXISTS public.sewer (
+        year integer,
+        cocaine numeric,
+        amphetamine numeric,
+        MDMA numeric,
+        methamphetamine numeric
+    )'''
+
+
+d = '''CREATE TABLE IF NOT EXISTS public.drug_prevalence_count (
+        case_id varchar(100) NOT NULL,
+        court_datum date, 
+        safrol integer,
+        piperonal integer,
+        PMK integer,
+        BMK integer,
+        apaan integer,
+        GBL integer, 
+        MDP2P integer,
+        efedrine integer, 
+        ergotamine integer,
+        lyserginezuur integer,
+        methamphetamine integer,
+        MDMA integer,
+        amphetamine integer,
+        cocaine integer,
+        XTC integer,
+        GHB integer,
+        PRIMARY KEY (case_id)
+    )'''
+
+queries = [a, b, c, d]
